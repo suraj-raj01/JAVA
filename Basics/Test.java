4180da05262b19769de1b8841380577b109b80e5
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-02 15:27:09 +0530

    Java Programming

---
+++ Basics/Test.java
@@ -0,0 +1,9 @@
+package Basics;
+
+class Test{
+	public static void main(String args[]){
+		System.out.println("Hello World!!");
+		System.out.printf("Hello World!!\n");
+		System.out.println("Hello World!!");
+	}
+}
