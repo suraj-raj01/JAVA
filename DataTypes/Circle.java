77609776c8bb6fa6d4d5e35f18022d093b020fc9
SURAJ_KUMAR <surajk38656@gmail.com>
2024-05-04 02:12:51 +0530

    Java Programming

--- DataTypes/Circle.java
+++ DataTypes/Circle.java
@@ -1,7 +1,8 @@
 // WAP to calculate area and circumference of circle.
-
+/*
 class Circle{
 	public static void main(String args[]){
+		// Using float DataType
 		float r,ar,cr;
 		r = 2.5f;
 		// Initialization
@@ -12,4 +13,18 @@ class Circle{
 		System.out.println("Circumference of Circle: "+cr);
 	}
 }
-		
+*/	
+
+class Circle{
+	public static void main(String args[]){
+		// Using double DataType
+		double r,ar,cr;
+		r = 2.5d;
+		// Initialization
+		ar = 3.14d*r*r;
+		cr = 2*3.14d*r;
+		// Operation
+		System.out.println("Area of Circle: "+ar);
+		System.out.println("Circumference of Circle: "+cr);
+	}
+}	
