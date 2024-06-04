114a5cbf0ceba2553756fe63878dd661ca98c445
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-04 18:33:11 +0530

    String

--- String/Str.java
+++ String/Str.java
@@ -1,19 +1,38 @@
-public class Str {
-    public static void main(String[] args) {
-        String s = new String("SURAJ");
-	//s = "SURAJ";
-        System.out.println(s);
+// == operator and equals() method in java programming
 
-	char arr[] = {'A','B','C','D'};
-	String c = new String(arr);
-	System.out.println(c);
-	
-	byte d[] = {65,66,67,68,69};
-	String b = new String(d,0,4);
-	System.out.println(b);
-	
-	String n = new String("WELCOME");
-	char x = n.charAt(5);
-	System.out.println(x);
+class Str{
+    public static void main(String[] args) {
+        String a = new String("HELLO");
+        String b = new String("HELLO");
+        String c = b;
+        System.out.println(a==b);  // false
+        System.out.println(a.equals(b));  // true
+        System.out.println(c==b);   // true
+
+        // Only One Object Will be create
+        String d = "HI";
+        String e = "HI";
+        String f = e;
+        System.out.println(d==e);
+        System.out.println(d.equals(e));
+        System.out.println(f==e);
+
+        // equal ignore case
+        System.out.println("Igone Case");
+        String x = "HELLO";
+        String y = "hello";
+        System.out.println(x.equals(y));
+        System.out.println(x.equalsIgnoreCase(y));
+
+        // Wrapper class
+        Integer i = new Integer("10");
+        Integer j = new Integer("10");
+        System.out.println(i==j);
+        System.out.println(i.equals(j));
+
+        // isEmpty()
+        // it will return true given String is empty otherwise return false
+        String s = "";
+        System.out.println(s.isEmpty());
     }
 }
