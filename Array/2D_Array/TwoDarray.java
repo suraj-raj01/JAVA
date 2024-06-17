e3554742830d85ca1a260f6aaa83dea2d89d9795
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-18 02:12:48 +0530

    Array type question

--- Array/2D_Array/TwoDarray.java
+++ Array/2D_Array/TwoDarray.java
@@ -1,3 +1,4 @@
+/*
 public class TwoDarray {
     public static void main(String[] args) {
         int arr[][] = new int[3][3];
@@ -19,4 +20,20 @@ public class TwoDarray {
             System.out.println();
         }
     }    
+}
+*/
+
+public class TwoDarray{
+	public static void main(String args[])
+	{
+	int arr[][] = {
+	{4,7,8},
+	{6,4,2},
+	{9,2,7}
+	};
+
+	System.out.println(arr[0][0]+" "+arr[0][1]+" "+arr[0][2]);
+	System.out.println(arr[1][0]+" "+arr[1][1]+" "+arr[1][2]);
+	System.out.println(arr[2][0]+" "+arr[2][1]+" "+arr[2][2]);
+	}
 }
