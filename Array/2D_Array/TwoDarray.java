4282993459ecf0abb31d4ca44812f7d4f94e800d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-18 15:03:56 +0530

    Array

--- Array/2D_Array/TwoDarray.java
+++ Array/2D_Array/TwoDarray.java
@@ -23,6 +23,7 @@ public class TwoDarray {
 }
 */
 
+/*
 public class TwoDarray{
 	public static void main(String args[])
 	{
@@ -36,4 +37,38 @@ public class TwoDarray{
 	System.out.println(arr[1][0]+" "+arr[1][1]+" "+arr[1][2]);
 	System.out.println(arr[2][0]+" "+arr[2][1]+" "+arr[2][2]);
 	}
+}
+*/
+
+import java.util.Scanner;
+class TwoDarray{
+public static void main(String args[]){
+int A[][]=new int[3][3];
+Scanner obj=new Scanner(System.in);
+System.out.println("Enter Element Index of 00 : ");
+A[0][0]=obj.nextInt();
+System.out.println("Enter Element Index of 01 : ");
+A[0][1]=obj.nextInt();
+System.out.println("Enter Element Index of 02 : ");
+A[0][2]=obj.nextInt();
+
+System.out.println("Enter Element Index of 10 : ");
+A[1][0]=obj.nextInt();
+System.out.println("Enter Element Index of 11 : ");
+A[1][1]=obj.nextInt();
+System.out.println("Enter Element Index of 12 : ");
+A[1][2]=obj.nextInt();
+
+System.out.println("Enter Element Index of 20 : ");
+A[2][0]=obj.nextInt();
+System.out.println("Enter Element Index of 21 : ");
+A[2][1]=obj.nextInt();
+System.out.println("Enter Element Index of 22 : ");
+A[2][2]=obj.nextInt();
+
+System.out.println(A[0][0]+"\t"+A[0][1]+"\t"+A[0][2]);
+System.out.println(A[1][0]+"\t"+A[1][1]+"\t"+A[1][2]);
+System.out.println(A[2][0]+"\t"+A[2][1]+"\t"+A[2][2]);
+obj.close();
+}
 }
---
+++ Array/SeconLargest.java
@@ -0,0 +1,36 @@
+// Write a java program to find second largest element from an array.
+
+import java.util.Scanner;
+class SecondLargest{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the size of an array: ");
+	int n = sc.nextInt();
+	int arr[] = new int[n];
+	int max=arr[0];
+	for(int i=0; i<arr.length; i++)
+	{
+	arr[i] = sc.nextInt();
+	}
+
+	for(int i=0; i<arr.length; i++)
+	{
+	if(arr[i]>max)
+	{
+	max = arr[i];
+	}
+	}
+	System.out.println("Maximum Element: "+max);
+	
+	int max1 = arr[0];
+	for(int i=0; i<arr.length; i++)
+	{
+	if(arr[i]>max1 && arr[i]<max)
+	{
+	max1 = arr[i];
+	}
+	}
+	System.out.println("second Largest Element : "+max1);
+	}
+}
