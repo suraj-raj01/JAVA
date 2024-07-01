6f28d54bce474b7a07c497386abc233b9a10ab8e
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-01 23:56:28 +0530

    StringBasedTask4

---
+++ StringBasedTask4/Q1.java
@@ -0,0 +1,26 @@
+/*
+1. Write a C program to check whether a given substring is present in the
+given string.
+Test Data :
+Input the string : This is a test string.
+Input the substring to be search : search
+Expected Output :
+The substring is not exists in the string.
+*/
+
+class Q1{
+	public static void main(String args[])
+	{
+	String s = new String("This is a test string");
+	for(int i=0; i<s.length(); i++)
+	{
+	System.out.print(s.charAt(i));
+	} 
+	
+	String sk = str.split();
+	for(int i=0; i<sk; i++)
+	{S
+	System.out.println(array.length);
+	}
+	}
+}
---
+++ StringBasedTask4/Q2.java
@@ -0,0 +1,13 @@
+/*
+2. Write a program in java to read a sentence and replace lowercase
+characters by uppercase and vice-versa. 
+Test Data :
+Input the string : This Is A Test String.
+Expected Output :
+The given sentence is : This Is A Test String.
+After Case changed the string is: tHIS iS a tEST sTRING.
+*/
+
+import java.util.Scanner;
+class demo{
+`	public static void main(String args];
