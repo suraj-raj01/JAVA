392d134f085de3f173f37776d26c27bbeb47d6df
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-24 00:49:55 +0530

    DSA->LinkedList

--- DSA-JAVA/LinkedList/LinkedList.java
+++ DSA-JAVA/LinkedList/LinkedList.java
@@ -1,6 +1,59 @@
 public class LinkedList {
-
+    int data;
+    LinkedList next;
+    public
+    LinkedList(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+    public LinkedList AddStart(LinkedList head, int data)
+    {
+        LinkedList newNode = new LinkedList(data);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+    public LinkedList AddEnd(LinkedList head,int data)
+    {
+        LinkedList newNode = new LinkedList(data);
+        if(head==null)
+        {
+            head = newNode;
+            return head;
+        }
+        else{
+            LinkedList temp = head;
+            while(temp.next!=null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        return head;
+    }
+    public void printData(LinkedList head)
+    {
+        if(head==null)
+        {
+            System.out.println("list is empty");
+        }
+        else{
+            LinkedList temp = head;
+            while (temp!=null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
     public static void main(String[] args) {
-
+        LinkedList first = new LinkedList(15);
+        LinkedList second = new LinkedList(20);
+        LinkedList head = first;
+        head.next = second;
+        head = head.AddStart(head, 10);
+        head.printData(head);
+        head = head.AddEnd(head, 25);
+        head.printData(head);
     }
 }
