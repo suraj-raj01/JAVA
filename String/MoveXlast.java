195ccc2b84664a068527844ce612453ec4e4c9f5
SURAJ_KUMAR <surajk38656@gmail.com>
2024-08-01 15:29:30 +0530

    String

---
+++ String/MoveXlast.java
@@ -0,0 +1,28 @@
+// Write a java program to move all 'X' in the end of a string.
+
+public class MoveXlast{
+    public static void moveAllx(String s)
+    {
+        int size = s.length();
+        char[] str = s.toCharArray();
+        int l=0;
+        for(int i=0; i<size; i++)
+        {
+            if(str[i]!='X' && str[i]!='x')
+            {
+                char temp = str[i];
+                str[i] = str[l];
+                str[l] = temp;
+                l++;
+            }
+        }
+        for(int i=0; i<size; i++)
+        {
+            System.out.print(str[i]);
+        }
+    }
+    public static void main(String[] args) {
+        String s = "sxkdxwxsxkx";
+        moveAllx(s);
+    }
+}
